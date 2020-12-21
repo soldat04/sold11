4e7733cbb767bcdb86834d8a01dcd66af74fefc9
soldat04 <76211233+soldat04@users.noreply.github.com>
2020-12-22 00:05:06 +0300

    Add files via upload

---
+++ labr/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labr
+{
+
+    public class Сlass1
+    { //сформировать n целых чисел от 2 до 14
+      //найти суммц чисел < 8
+      //вывести результат на экран
+
+        
+        public int[] Rand(int countNumbers, int minValue = 2, int maxValue = 14)
+        {
+            Random rand = new Random();
+            int[] ms = new int[countNumbers];
+            for (int i = 0; i < ms.Length; i++)
+            {
+                ms[i] = rand.Next(minValue, maxValue);
+            }
+            return ms;
+        }
+
+
+        //находим сумму < 8
+        public int Summa(int[] mass)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] < 8)
+                    sum += mass[i];
+            }
+            return sum;
+
+
+        }
+
+        
+    }
+   
+
+}
+    
+
---
+++ labr/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using labr;
+
+namespace soldat1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+              Сlass1 сlass1 = new Сlass1();
+              int count = (int)numericUpDown1.Value;
+              
+
+
+
+
+
+
+               int[] ms = сlass1.Rand(count);
+                textBox2.Text = сlass1.Summa(ms).ToString();
+                textBox1.Clear();
+              for (int i = 0; i < ms.Length; i++)
+              {
+                textBox1.Text += ms[i].ToString() + " ";
+              }
+
+
+            }
+    
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Солдатова Анастасия ИСП-31\n" +
+            "Практическая работа №1\n" +
+            "9. Сформировать n целых чисел от 2 до 14, найти суммц чисел < 8, вывести результат на экран.\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
